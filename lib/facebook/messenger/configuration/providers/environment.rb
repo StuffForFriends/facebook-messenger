0753c1ea170167773bf70a87e1531c445b10d236
Scott Bronson <brons_git@rinspin.com>
2016-11-19 22:12:13 -0800

    add some temporary debugging

--- lib/facebook/messenger/configuration/providers/environment.rb
+++ lib/facebook/messenger/configuration/providers/environment.rb
@@ -5,6 +5,10 @@ module Facebook
         # Configuration provider for environment variables.
         class Environment
           def valid_verify_token?(verify_token)
+            System.out.println('VERIFY TOKEN:')
+            System.out.println('     got: ' + verify_token)
+            System.out.println('expected: ' + ENV['VERIFY_TOKEN'])
+            System.out.println('DONE VERIFY')
             verify_token == ENV['VERIFY_TOKEN']
           end
 
